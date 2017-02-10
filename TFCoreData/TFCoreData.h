912f69771712401adb6e604468178825a29e2632
Wojciech Nagrodzki <w.nagrodzki@gmail.com>
2017-02-10 20:15:52 +0100

    Adds initial Xcode project

---
+++ TFCoreData/TFCoreData.h
@@ -0,0 +1,19 @@
+//
+//  TFCoreData.h
+//  TFCoreData
+//
+//  Created by Wojciech Nagrodzki on 10/02/2017.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TFCoreData.
+FOUNDATION_EXPORT double TFCoreDataVersionNumber;
+
+//! Project version string for TFCoreData.
+FOUNDATION_EXPORT const unsigned char TFCoreDataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TFCoreData/PublicHeader.h>
+
+
